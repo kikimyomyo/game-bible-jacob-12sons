833dec5ccb56c587a6e9788051c6ef80449da634
kikimyomyo <request1004@gmail.com>
2026-04-18 14:52:05 +0900

    deploy: bible-jacob-12sons

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAME_CONFIG = {
+  game: "bible-jacob-12sons",
+  title: "야곱의 12아들 순서 맞추기",
+  sheet_tab: "bible-jacob-12sons",
+  endpoint: "https://script.google.com/macros/s/AKfycbx3a96h-vRywYTCxOJxdn199uaFAELyfzy7LRIGeInFuG-ZhxsBh_AKso--Yw8PbYPH/exec",
+};
